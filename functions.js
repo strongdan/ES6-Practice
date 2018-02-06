fe417e34a342e862346fd20660e53f781adbc2aa
Dan Strong <danielstrong@mac.com>
2018-02-05 20:51:34 -0900

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,1 +1,29 @@
 // covers arrow functions, "this", destructuring, classes, super, extends
+
+// ES5 Class
+function Car(numCylinders, color, make, model) {
+  this.numCylinders = numCylinders;
+  this.color = color;
+  this.make = make;
+  this.model = model;
+}
+
+Car.prototype.drive = function () {
+  console.log('driving...');
+};
+
+// ES6 Class
+class Car {
+  constructor(numCylinders, color, make, model) {
+    this.numCylinders = numCylinders;
+    this.color = color;
+    this.make = make;
+    this.model = model;
+  }
+
+  drive() {
+    console.log('driving…');
+  }
+}
+
+typeof Car; // => function
