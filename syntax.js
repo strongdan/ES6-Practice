596aa42658ceda9d5b321ed8407a943b5af26846
Dan Strong <danielstrong@mac.com>
2018-02-12 12:57:19 -0900

    Update syntax.js

--- syntax.js
+++ syntax.js
@@ -1,1 +1,5 @@
 // covers syntax changes, iteration, the spread operator and the rest parameter
+
+// Arrow Functions
+// Unlike functions, arrows share the same lexical this as their surrounding code. 
+// If an arrow is inside another function, it shares the “arguments” variable of its parent function.
